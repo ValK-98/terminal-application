1325a41d1b00cc77cc2170441c36880b899f4768
ValK-98 <vkornijenka@gmail.com>
2023-10-23 19:57:48 +1000

    added workout database and implemented basic generator

---
+++ workout_database.py
@@ -0,0 +1,58 @@
+workouts = [
+    {
+        "name": "squats",
+        "body_part": "legs",
+    },
+    {
+        "name": "leg extensions",
+        "body_part": "legs",
+    },
+    {
+        "name": "hamstring curl",
+        "body_part": "legs",
+    },
+    {
+        "name": "bench press",
+        "body_part": "chest",
+    },
+    {
+        "name": "incline dumbbell bench press",
+        "body_part": "chest",
+    },
+    {
+        "name": "cable fly",
+        "body_part": "chest",
+    },
+    {
+        "name": "bicep curl",
+        "body_part": "arms",
+    },
+    {
+        "name": "tricep extensions",
+        "body_part": "arms",
+    },
+    {
+        "name": "overhead press",
+        "body_part": "shoulders",
+    },
+    {
+        "name": "arnold press",
+        "body_part": "shoulders",
+    },
+    {
+        "name": "lat raises",
+        "body_part": "shoulders",
+    },
+    {
+        "name": "deadlift",
+        "body_part": "back",
+    },
+    {
+        "name": "lat pulldown",
+        "body_part": "back",
+    },
+    {
+        "name": "rows",
+        "body_part": "back",
+    },
+]
---
+++ workout_generator.py
@@ -0,0 +1,33 @@
+import workout_database
+import random
+
+# Create a workout plan
+workout_days = 5
+
+for session in range(workout_days):
+    # Shuffle the workouts list to randomize exercise selection for each session
+    random.shuffle(workout_database.workouts)
+    
+    workout_plan = []
+    body_parts_in_session = set()
+
+    while len(workout_plan) < 4:
+        for workout in workout_database.workouts:
+            body_part = workout["body_part"]
+            if body_part not in body_parts_in_session:
+                workout_plan.append(workout)
+                body_parts_in_session.add(body_part)
+
+            if len(workout_plan) == 4:
+                break  # Exit the inner loop if we have 4 exercises
+
+    print(f"Session {session + 1}:")
+    for exercise in workout_plan:
+        print(f"  {exercise['name']} ({exercise['body_part']})")
+    print()
+
+    # Decrease the remaining workout days
+    workout_days -= 1
+
+    if workout_days == 0:
+        break
