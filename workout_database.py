6e737b22cb7bed31cb6cf7a8a8183581fee85f56
ValK-98 <vkornijenka@gmail.com>
2023-10-23 20:55:17 +1000

    added workout 'type' in database

--- workout_database.py
+++ workout_database.py
@@ -2,105 +2,131 @@ workouts = [
     {
         "name": "squats",
         "body_part": "legs",
+        "type": "compound",
     },
     {
         "name": "leg extensions",
         "body_part": "legs",
+        "type": "accessory",
     },
     {
         "name": "hamstring curl",
         "body_part": "legs",
+        "type": "accessory",
     },
     {
         "name": "lunges",
         "body_part": "legs",
+        "type": "accessory",
     },
     {
         "name": "leg press",
         "body_part": "legs",
+        "type": "compound",
     },
-     {
+    {
         "name": "calf raises",
         "body_part": "legs",
+        "type": "accessory",
     },
     {
         "name": "bench press",
         "body_part": "chest",
+        "type": "compound",
     },
     {
         "name": "push-ups",
         "body_part": "chest",
+        "type": "compound",
     },
     {
         "name": "dips",
         "body_part": "chest",
+        "type": "accessory",
     },
     {
         "name": "pec deck fly",
         "body_part": "chest",
+        "type": "accessory",
     },
     {
         "name": "incline dumbbell bench press",
         "body_part": "chest",
+        "type": "compound",
     },
     {
         "name": "cable fly",
         "body_part": "chest",
+        "type": "accessory",
     },
     {
         "name": "bicep curl",
         "body_part": "arms",
+        "type": "accessory",
     },
     {
         "name": "hammer curls",
         "body_part": "arms",
+        "type": "accessory",
     },
-     {
+    {
         "name": "skull crushers",
         "body_part": "arms",
+        "type": "accessory",
     },
     {
         "name": "tricep extensions",
         "body_part": "arms",
+        "type": "accessory",
     },
     {
         "name": "overhead press",
         "body_part": "shoulders",
+        "type": "compound",
     },
     {
         "name": "front raises",
         "body_part": "shoulders",
+        "type": "accessory",
     },
     {
         "name": "shrugs",
         "body_part": "shoulders",
+        "type": "accessory",
     },
     {
         "name": "arnold press",
         "body_part": "shoulders",
+        "type": "accessory",
     },
     {
         "name": "lat raises",
         "body_part": "shoulders",
+        "type": "accessory",
     },
     {
         "name": "deadlifts",
         "body_part": "back",
+        "type": "compound",
     },
     {
         "name": "bent-over rows",
         "body_part": "back",
+        "type": "compound",
     },
     {
         "name": "pull-ups",
         "body_part": "back",
+        "type": "compound",
     },
     {
         "name": "lat pulldown",
         "body_part": "back",
+        "type": "accessory",
     },
     {
         "name": "rows",
         "body_part": "back",
+        "type": "accessory",
     },
 ]
