9035477c53db394daee1ea908c7f23335e536d3a
ValK-98 <vkornijenka@gmail.com>
2023-10-24 19:44:07 +1000

    removed 'goal type' from workout_database.py

---
+++ workout_goals.py
@@ -0,0 +1,10 @@
+# # Function to determine the number of compound and accessory exercises based on the user's goal
+# def get_exercise_counts(goal_type):
+#     if goal_type == "muscle gain":
+#         compound_count = 2  # Fewer compound exercises for muscle gain
+#         accessory_count = 2  # More accessory exercises for muscle gain
+#     else:
+#         compound_count = 3  # More compound exercises for strength gain
+#         accessory_count = 1  # Fewer accessory exercises for strength gain
+
+#     return compound_count, accessory_count
