508cf078b0e4d7f223b1b4d954ba995151d4b5fc
ValK-98 <vkornijenka@gmail.com>
2023-10-24 18:41:01 +1000

    created user_inputs and moved 'get_valid_workout_days()'

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import workout_generator
+import user_inputs
 
 
 # Get the number of workout days from the user
 
-workout_days = workout_generator.get_valid_workout_days()
+workout_days = user_inputs.get_valid_workout_days()
 
 # Generate and display the workout plan
 
--- workout_generator.py
+++ workout_generator.py
@@ -2,18 +2,6 @@
 import workout_database
 import random
 
-# Function to get a valid number of workout days from the user
-def get_valid_workout_days():
-    while True:
-        try:
-            workout_days = int(input("Enter the number of workout days (1-5): "))
-            if 1 <= workout_days <= 5:
-                return workout_days
-            else:
-                print("Please enter a number between 1 and 5.")
-        except ValueError:
-            print("Please enter a valid integer between 1 and 5.")
-
 # Function to get a random exercise from a given list
 def get_random_exercise(exercise_list):
     random.shuffle(exercise_list)
