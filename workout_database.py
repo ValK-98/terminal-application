3bef6ca88f3afd3599408074547157da6c5cc2fb
ValK-98 <vkornijenka@gmail.com>
2023-10-25 20:24:05 +1000

    changed workout_database to output tuple, changed workout_generator to dot notation

--- workout_database.py
+++ workout_database.py
@@ -1,132 +1,32 @@
+from collections import namedtuple
+
+Exercise = namedtuple('Exercise', ['name', 'body_part', 'type'])
+
 workouts = [
-    {
-        "name": "squats",
-        "body_part": "legs",
-        "type": "compound",
-    },
-    {
-        "name": "leg extensions",
-        "body_part": "legs",
-        "type": "accessory",
-    },
-    {
-        "name": "hamstring curl",
-        "body_part": "legs",
-        "type": "accessory",
-    },
-    {
-        "name": "lunges",
-        "body_part": "legs",
-        "type": "accessory",
-    },
-    {
-        "name": "leg press",
-        "body_part": "legs",
-        "type": "compound",
-    },
-    {
-        "name": "calf raises",
-        "body_part": "legs",
-        "type": "accessory",
-    },
-    {
-        "name": "bench press",
-        "body_part": "chest",
-        "type": "compound",
-    },
-    {
-        "name": "push-ups",
-        "body_part": "chest",
-        "type": "compound",
-    },
-    {
-        "name": "dips",
-        "body_part": "chest",
-        "type": "accessory",
-    },
-    {
-        "name": "pec deck fly",
-        "body_part": "chest",
-        "type": "accessory",
-    },
-    {
-        "name": "incline dumbbell bench press",
-        "body_part": "chest",
-        "type": "compound",
-    },
-    {
-        "name": "cable fly",
-        "body_part": "chest",
-        "type": "accessory",
-    },
-    {
-        "name": "bicep curl",
-        "body_part": "arms",
-        "type": "accessory",
-    },
-    {
-        "name": "hammer curls",
-        "body_part": "arms",
-        "type": "accessory",
-    },
-    {
-        "name": "skull crushers",
-        "body_part": "arms",
-        "type": "accessory",
-    },
-    {
-        "name": "tricep extensions",
-        "body_part": "arms",
-        "type": "accessory",
-    },
-    {
-        "name": "overhead press",
-        "body_part": "shoulders",
-        "type": "compound",
-    },
-    {
-        "name": "front raises",
-        "body_part": "shoulders",
-        "type": "accessory",
-    },
-    {
-        "name": "shrugs",
-        "body_part": "shoulders",
-        "type": "accessory",
-    },
-    {
-        "name": "arnold press",
-        "body_part": "shoulders",
-        "type": "accessory",
-    },
-    {
-        "name": "lat raises",
-        "body_part": "shoulders",
-        "type": "accessory",
-    },
-    {
-        "name": "deadlifts",
-        "body_part": "back",
-        "type": "compound",
-    },
-    {
-        "name": "bent-over rows",
-        "body_part": "back",
-        "type": "compound",
-    },
-    {
-        "name": "pull-ups",
-        "body_part": "back",
-        "type": "compound",
-    },
-    {
-        "name": "lat pulldown",
-        "body_part": "back",
-        "type": "accessory",
-    },
-    {
-        "name": "rows",
-        "body_part": "back",
-        "type": "accessory",
-    },
+    Exercise("squats", "legs", "compound"),
+    Exercise("leg extensions", "legs", "accessory"),
+    Exercise("hamstring curl", "legs", "accessory"),
+    Exercise("lunges", "legs", "accessory"),
+    Exercise("leg press", "legs", "compound"),
+    Exercise("calf raises", "legs", "accessory"),
+    Exercise("bench press", "chest", "compound"),
+    Exercise("push-ups", "chest", "compound"),
+    Exercise("dips", "chest", "accessory"),
+    Exercise("pec deck fly", "chest", "accessory"),
+    Exercise("incline dumbbell bench press", "chest", "compound"),
+    Exercise("cable fly", "chest", "accessory"),
+    Exercise("bicep curl", "arms", "accessory"),
+    Exercise("hammer curls", "arms", "accessory"),
+    Exercise("skull crushers", "arms", "accessory"),
+    Exercise("tricep extensions", "arms", "accessory"),
+    Exercise("overhead press", "shoulders", "compound"),
+    Exercise("front raises", "shoulders", "accessory"),
+    Exercise("shrugs", "shoulders", "accessory"),
+    Exercise("arnold press", "shoulders", "accessory"),
+    Exercise("lat raises", "shoulders", "accessory"),
+    Exercise("deadlifts", "back", "compound"),
+    Exercise("bent-over rows", "back", "compound"),
+    Exercise("pull-ups", "back", "compound"),
+    Exercise("lat pulldown", "back", "accessory"),
+    Exercise("rows", "back", "accessory"),
 ]
--- workout_generator.py
+++ workout_generator.py
@@ -4,12 +4,12 @@ from sets_reps_generator import SetsRepsGenerator
 
 class WorkoutGenerator:
     def __init__(self):
-        self.body_parts = set(exercise["body_part"] for exercise in workout_database.workouts)
-        self.compound_exercises = [exercise for exercise in workout_database.workouts if exercise["type"] == "compound"]
-        self.accessory_exercises = [exercise for exercise in workout_database.workouts if exercise["type"] == "accessory"]
+        self.body_parts = set(exercise.body_part for exercise in workout_database.workouts)
+        self.compound_exercises = [exercise for exercise in workout_database.workouts if exercise.type == "compound"]
+        self.accessory_exercises = [exercise for exercise in workout_database.workouts if exercise.type == "accessory"]
 
     def get_random_exercise(self, exercise_list, excluded_names=set()):
-        exercise_list = [e for e in exercise_list if e["name"] not in excluded_names]
+        exercise_list = [e for e in exercise_list if e.name not in excluded_names]
         random.shuffle(exercise_list)
         return exercise_list.pop() if exercise_list else None
 
@@ -31,28 +31,27 @@ class WorkoutGenerator:
                 compound_exercise = self.get_random_exercise(compound_exercises_copy, used_compound_exercises)
                 if compound_exercise:
                     workout_plan.append(compound_exercise)
-                    used_compound_exercises.add(compound_exercise["name"])
+                    used_compound_exercises.add(compound_exercise.name)
         else:
             compound_exercise = self.get_random_exercise(compound_exercises_copy, used_compound_exercises)
             if compound_exercise:
                 workout_plan.append(compound_exercise)
-                used_compound_exercises.add(compound_exercise["name"])
-                body_part_counts[compound_exercise["body_part"]] += 1
+                used_compound_exercises.add(compound_exercise.name)
+                body_part_counts[compound_exercise.body_part] += 1
 
             while len(workout_plan) < 4:
-                # Give preference to body parts that haven't been worked out as much
                 underworked_body_parts = [part for part, count in body_part_counts.items() if count == min(body_part_counts.values())]
-                possible_exercises = [ex for ex in accessory_exercises_copy if ex["body_part"] in underworked_body_parts]
+                possible_exercises = [ex for ex in accessory_exercises_copy if ex.body_part in underworked_body_parts]
                 accessory_exercise = self.get_random_exercise(possible_exercises)
                 
                 if accessory_exercise:
                     workout_plan.append(accessory_exercise)
-                    body_part_counts[accessory_exercise["body_part"]] += 1
+                    body_part_counts[accessory_exercise.body_part] += 1
 
         print(f"Day {day}:")
         for exercise in workout_plan:
-            sets, reps = self.sets_reps_generator.get_sets_reps(exercise['type'])
-            print(f"  {exercise['name']} ({exercise['body_part']}) - {reps} reps, {sets} sets")
+            sets, reps = self.sets_reps_generator.get_sets_reps(exercise.type)
+            print(f"  {exercise.name} ({exercise.body_part}) - {reps} reps, {sets} sets")
         print()
 
 
