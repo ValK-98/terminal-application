2e55ddaf1c2023fc2caa559b3ebb2981ebe8cce6
ValK-98 <vkornijenka@gmail.com>
2023-10-26 13:51:45 +1000

    added ability for user to remove and swap exercises

--- workout_displayer.py
+++ workout_displayer.py
@@ -34,3 +34,73 @@ class WorkoutDisplayer:
                     print("Invalid option. Try again.")
             except Exception as e:
                 print(f"An error occurred: {e}. Please try again.")
+
+    def _get_input_within_range(self, prompt, upper_bound, lower_bound=1):
+        while True:
+            try:
+                value = int(input(prompt))
+                if lower_bound <= value <= upper_bound:
+                    return value
+                else:
+                    print(f"Please enter a number between {lower_bound} and {upper_bound}.")
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+
+    def _get_valid_day(self, workout_schedule):
+        try:
+            return self._get_input_within_range("Enter the day number: ", len(workout_schedule))
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None
+
+    def _list_exercises(self, workout_day):
+        try:
+            for idx, exercise in enumerate(workout_day["Exercises"], start=1):
+                print(f"{idx}. {exercise['Name']}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    def _get_valid_exercise(self, workout_day, action="modify"):
+        try:
+            self._list_exercises(workout_day)
+            return self._get_input_within_range(f"Choose an exercise number to {action}: ", len(workout_day["Exercises"])) - 1
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None
+
+    def _get_valid_day_and_exercise(self, workout_schedule, action="modify"):
+        try:
+            day = self._get_valid_day(workout_schedule)
+            if day is None:
+                return None, None
+            exercise_num = self._get_valid_exercise(workout_schedule[day-1], action)
+            if exercise_num is None:
+                return None, None
+            return day, exercise_num
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None, None
+
+    def remove_workout(self, workout_schedule):
+        try:
+            day, to_remove = self._get_valid_day_and_exercise(workout_schedule, "remove")
+            if day and to_remove is not None:
+                del workout_schedule[day-1]['Exercises'][to_remove]
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    def swap_workout(self, workout_schedule):
+        try:
+            day, to_swap = self._get_valid_day_and_exercise(workout_schedule, "swap")
+            if day and to_swap is not None:
+                swap_with_body_part = input("Enter the body part to swap with (or type 'same' for the same body part): ")
+                if swap_with_body_part == 'same':
+                    swap_with_body_part = workout_schedule[day-1]['Exercises'][to_swap]['Body Part']
+                new_exercise = next(e for e in workout_database.workouts if e.body_part == swap_with_body_part and e.name not in [ex['Name'] for ex in workout_schedule[day-1]['Exercises']])
+                workout_schedule[day-1]['Exercises'][to_swap]['Name'] = new_exercise.name
+                workout_schedule[day-1]['Exercises'][to_swap]['Body Part'] = new_exercise.body_part
+        except StopIteration:
+            print("No available exercises to swap with. Try another body part.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
