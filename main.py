f7a38d664620891223b93670926fd8e77fdd34f9
ValK-98 <vkornijenka@gmail.com>
2023-10-26 12:59:31 +1000

    added try/except for main.py

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ from workout_generator import WorkoutGenerator
 from workout_generator import WorkoutDisplayer
 import user_inputs
 
-# Get the user's goal type ("muscle gain" or "strength gain")
-goal_type = user_inputs.get_goal_type()
 
-# Get the number of workout days from the user
-workout_days = user_inputs.get_valid_workout_days()
+try:
+    # Get the user's goal type ("muscle gain" or "strength gain")
+    goal_type = user_inputs.get_goal_type()
 
-# Create an instance of WorkoutGenerator
-generator = WorkoutGenerator()
-workout_schedule = generator.generate_workout(workout_days, goal_type)
+    # Get the number of workout days from the user
+    workout_days = user_inputs.get_valid_workout_days()
 
-#Display the workout plan
-displayer = WorkoutDisplayer()
-displayer.display_workout_schedule(workout_schedule)
+    # Create an instance of WorkoutGenerator
+    generator = WorkoutGenerator()
+    workout_schedule = generator.generate_workout(workout_days, goal_type)
+
+    #Display the workout plan
+    displayer = WorkoutDisplayer()
+    displayer.display_workout_schedule(workout_schedule)
 
 
-# print(workout_schedule)
+    # print(workout_schedule)
+
+except Exception as e:
+    print(f"An error occurred: {e}")
