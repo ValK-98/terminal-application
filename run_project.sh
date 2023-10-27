dd44904535caf658b7c2a71c71ff84aad710398d
ValK-98 <vkornijenka@gmail.com>
2023-10-27 18:00:41 +1000

    fixed shell not running main.py

--- run_project.sh
+++ run_project.sh
@@ -5,6 +5,13 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+# Display help message
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    echo "Usage: $0"
+    echo "Runs the Workout Application. Make sure you have Python 3 installed."
+    exit 0
+fi
+
 # Check if python3 is installed
 command -v python3 &>/dev/null || {
     echo -e "${RED}Python 3 is required but it's not installed. Exiting.${NC}"
@@ -23,6 +30,9 @@ fi
 
 # Welcome message
 echo -e "${GREEN}Welcome to the Workout Application!${NC}"
+
+# Run the main.py script using Python 3 and log the output
+python3 main.py 2>&1
 
 # Exit message
 echo -e "${GREEN}Thank you for using the Workout Application! Have a great day!${NC}"
